d05089dd9fd14dadd6a11129915aa7f8568636fd
AnastasiiaMarchuk <stasijushka5@gmail.com>
2023-08-19 07:23:35 +0300

    fixed

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import { LoadMoreButton } from './Button.styled';
 
-export const Button = ({ onClick, loading }) => {
+export const Button = ({ onClick }) => {
   return (
     <LoadMoreButton onClick={onClick} type="button">
       Load more
